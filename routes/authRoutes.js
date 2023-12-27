30bfe936567bda237ff75cf0c4d0f83306b265fe
Vivek Thakur <vivekthakur9810@gmail.com>
2023-12-28 02:34:50 +0530

    added forget and reset password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,7 @@ router.post("/login", authController.login);
 router.post("/email", authController.signUpWithEmail);
 router.post("/email/verify", authController.emailVerify);
 router.post("/signup", authVerifyToken, authController.signUpTwo);
+router.post("/forget", authController.forgetPassword);
+router.post("/reset", authController.resetPassword);
 
 module.exports = router;
