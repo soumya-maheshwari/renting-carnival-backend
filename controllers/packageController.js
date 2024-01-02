7c84d81eea2bd7e58377000603559d4ca502c6bb
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2024-01-03 02:01:56 +0530

    fixed error

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -3,7 +3,7 @@ const Package = require("../models/packageModel");
 const getAllPackages = async (req, res, next) => {
   try {
     const packages = await Package.find();
-    return res.satus(200).json({
+    return res.status(200).json({
       success: true,
       msg: "All packages",
       packages,
