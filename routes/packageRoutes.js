02c6d718c167b60b14ed55fec1cb54a010d9cc99
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2024-01-12 15:11:09 +0530

    remove token

--- routes/packageRoutes.js
+++ routes/packageRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { packageController } = require("../controllers");
 const { authVerifyToken } = require("../middleware/authVerifyToken");
 
-router.get("/all", authVerifyToken, packageController.getAllPackages);
+router.get("/all", packageController.getAllPackages);
 router.get("/:packageId", authVerifyToken, packageController.getSinglePackage);
 router.post("/update", authVerifyToken, packageController.updatePackage);
 
